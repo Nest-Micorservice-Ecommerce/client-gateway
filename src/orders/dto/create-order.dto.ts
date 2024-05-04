d052facafed9e2bc18b9321989a8ad607669fa6b
Dieguidev <diegogaraycullas@gmail.com>
2024-05-04 12:10:23 -0500

    creado creado el order-item.dto.ts y mejorado el create-order.dto.ts

--- src/orders/dto/create-order.dto.ts
+++ src/orders/dto/create-order.dto.ts
@@ -1,27 +1,36 @@
 
-import { IsBoolean, IsEnum, IsNumber, IsOptional, IsPositive } from "class-validator";
-import { OrderStatus, OrderStatusList } from "../enum/order.enum";
+import { ArrayMinSize, IsArray, ValidateNested } from "class-validator";
+
 import { Type } from "class-transformer";
+import { OrderItemDto } from "./order-item.dto";
 
 
 export class CreateOrderDto {
-  @IsNumber()
-  @IsPositive()
-  @Type(() => Number)
-  totalAmount: number;
 
-  @IsNumber()
-  @IsPositive()
-  @Type(() => Number)
-  totalItems: number;
+  @IsArray()
+  @ArrayMinSize(1)
+  @ValidateNested({ each: true })
+  @Type(() => OrderItemDto)
+  items: OrderItemDto[]
 
-  @IsEnum(OrderStatusList, {
-    message: `Possible status values are ${OrderStatusList}`
-  })
-  @IsOptional()
-  status: OrderStatus = OrderStatus.PENDING
 
-  @IsBoolean()
-  @IsOptional()
-  paid: boolean = false;
+  // @IsNumber()
+  // @IsPositive()
+  // @Type(() => Number)
+  // totalAmount: number;
+
+  // @IsNumber()
+  // @IsPositive()
+  // @Type(() => Number)
+  // totalItems: number;
+
+  // @IsEnum(OrderStatusList, {
+  //   message: `Possible status values are ${OrderStatusList}`
+  // })
+  // @IsOptional()
+  // status: OrderStatus = OrderStatus.PENDING
+
+  // @IsBoolean()
+  // @IsOptional()
+  // paid: boolean = false;
 }
